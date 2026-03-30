0f5ab9edbdbc7519aa3112437dc0fd84322d3741
Marcello Andrade Rodrigues dos Santos <824216008@ulife.com.br>
2026-03-30 20:32:53 -0300

    tudo dnv

--- previsao_do_tempo/index.js
+++ previsao_do_tempo/index.js
@@ -7,4 +7,16 @@ const cnt = 2
 const baseURL = `https://api.openweathermap.org/data/2.5/forecast`
 const url = `${baseURL}?q=${q}&appid=${appid}&units=${units}&lang=${lang}&cnt=${cnt}`
 const minhaPromise = axios.get(url)
-minhaPromise.then(res => console.log(res))
+minhaPromise.then(res => {
+    console.log(res.data)
+    console.log('===========')
+    return res.data
+})
+.then(res=> {
+    console.log(`cnt: ${cnt}`)
+    console.log('===========')
+})
+.then(res => {
+    //pegar o main do primeiro elemento da lista
+    
+})
